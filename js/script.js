ec65f4205d981742cff34b24d781daf36289173e
Miles_Morales <84193955+MilesGonzaloMorales@users.noreply.github.com>
2023-09-09 15:15:05 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-// **************JS FOR #treatment-slider START*********************************************************
+//*********************JAVASCRIPT FOR SLIDER HOME PAGE START****************************************************
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+//*********************JAVASCRIPT FOR SLIDER HOME PAGE END****************************************************
+
+// **********************JS FOR TREATMENT SLIDER START*********************************************************
 var swiper = new Swiper(".slide-container", {
     slidesPerView: 4,
     spaceBetween: 20,
@@ -13,8 +30,8 @@ var swiper = new Swiper(".slide-container", {
         dynamicBullets: true,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next-treatment",
+        prevEl: ".swiper-button-prev-treatment",
     },
 
     breakpoints: {
@@ -32,4 +49,39 @@ var swiper = new Swiper(".slide-container", {
         },
     },
 });
-// **************JS FOR #treatment-slider END*********************************************************
+// **************JS FOR TREATMENT SLIDER END*********************************************************
+
+// ***********************************JAVASCRIPT FOR THE TESTIMONIAL SLIDER START **************************************************
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next-testimonial",
+        prevEl: ".swiper-button-prev-testimonial",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 4,
+        },
+    },
+});
+    // ***********************************JAVASCRIPT FOR THE TESTIMONIAL SLIDER END **************************************************
